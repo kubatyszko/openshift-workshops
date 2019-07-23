e7b27aea97fac460059f386ef87f5c3c258776a5
Kuba Tyszko <kt@zestfinance.com>
2019-07-23 10:10:30 -0700

    meh
    te

--- dc-metro-map/test/dc-metro-map_test.js
+++ dc-metro-map/test/dc-metro-map_test.js
@@ -4,6 +4,6 @@ exports.passOnOddMinute = function(test) {
   var date = new Date();
   var minutes = date.getMinutes();
     
-  test.ok(minutes % 2 === 1, "this assertion should pass");
+  test.ok(minutes % 2 === 1, "this assertion should probably pass");
   test.done();
 };
